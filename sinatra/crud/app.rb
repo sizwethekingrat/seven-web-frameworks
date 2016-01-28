b151f0f03f05e558698c79e8b3a82ffcf369a5c7
Mike Hewitson <mike@hewitson.co.za>
2016-01-28 17:03:46 +0200

    Busy with CRUD

--- sinatra/crud/app.rb
+++ sinatra/crud/app.rb
@@ -1,21 +1,20 @@
 #---
 # Excerpted from "Seven Web Frameworks in Seven Weeks",
 # published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
+# Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/7web for more book information.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/7web for more book information
 #---
-require "sinatra"
-require "data_mapper"
-require_relative "bookmark"
-require "dm-serializer"
-
+require 'sinatra'
+require 'data_mapper'
+require_relative 'bookmark'
+require 'dm-serializer'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/bookmarks.db")
 DataMapper.finalize.auto_upgrade!
 
-get "/bookmarks/:id" do
+get '/bookmarks/:id' do
   id = params[:id]
   bookmark = Bookmark.get(id)
   content_type :json
@@ -36,16 +35,16 @@ delete "/bookmarks/:id" do
   bookmark.destroy
   200 # OK
 end
-def get_all_bookmarks
-  Bookmark.all(:order => :title)
+def fetch_all_bookmarks
+  Bookmark.all(order: :title)
 end
-get "/bookmarks" do
+get '/bookmarks' do
   content_type :json
-  get_all_bookmarks.to_json
+  fetch_all_bookmarks.to_json
 end
 
-post "/bookmarks" do
-  input = params.slice "url", "title"
+post '/bookmarks' do
+  input = params.slice 'url', 'title'
   bookmark = Bookmark.create input
   # Created
   [201, "/bookmarks/#{bookmark['id']}"]
--- sinatra/crud/bookmark.rb
+++ sinatra/crud/bookmark.rb
@@ -1,13 +1,12 @@
+require 'data_mapper'
 #---
 # Excerpted from "Seven Web Frameworks in Seven Weeks",
 # published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
+# Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/7web for more book information.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/7web for more book information
 #---
-require "data_mapper"
-
 class Bookmark
   include DataMapper::Resource
 
